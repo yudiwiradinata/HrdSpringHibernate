60ae4a75c7001295df9f2ad970e6212b5da74e89
Yudi Wiradinata <yudiwiradinata@gmail.com>
2014-07-10 15:29:46 +0700

    update hello world spring MVC 4

---
+++ src/main/java/com/yudi/project/hrd/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.yudi.project.hrd.controller;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String printWelcome(@RequestParam String nameString @Required=false, ModelMap model){
+	
+		model.addAttribute("message", "Spring 4 hello World");
+		
+		return "hello";
+		
+	}
+}
---
+++ src/main/java/com/yudi/project/hrd/model/User.java
@@ -0,0 +1,13 @@
+package com.yudi.project.hrd.model;
+
+public class User {
+
+	private String username;
+	private String password;
+	private String salt;
+	
+	
+	
+	
+	
+}
